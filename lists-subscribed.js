7385599d8b8bda032dfa4870697055482434fe1e
Sx112233 <83616215+Sx112233@users.noreply.github.com>
2021-05-06 12:47:48 +0300

    Azure
    Pipelines successfully started running 1 pipeline(s).

---
+++ lists-subscribed.js
@@ -0,0 +1,13 @@
+window.YTD.lists_subscribed.part0 = [ {
+  "userListInfo" : {
+    "url" : "https://twitter.com/AhmadQahtani/lists/list1"
+  }
+}, {
+  "userListInfo" : {
+    "url" : "https://twitter.com/mufarrah67/lists/list2"
+  }
+}, {
+  "userListInfo" : {
+    "url" : "https://twitter.com/eng_akareem/lists/khobar"
+  }
+} ]
